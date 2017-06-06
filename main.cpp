5465218a1a0db7a8cf3beb7443fead25eac5aba2
lovenery <edittye379@gmail.com>
2017-06-06 16:30:26 +0800

    :sparkles: motor

--- main.cpp
+++ main.cpp
@@ -7,85 +7,104 @@
 #include "TCPServer.h"
 #include "TCPSocket.h"
 #include "wdg.h"
-#include <cstdio>
-#include <cstdlib>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <map>
 
 Serial pc(SERIAL_TX, SERIAL_RX);
-DigitalOut led(LED1);
-DigitalOut led2(LED2);
-DigitalOut led3(LED3);
-DigitalIn btnSocketSend(USER_BUTTON);
 
 TCPServer srv;
 TCPSocket clt_sock;
 SocketAddress clt_addr;
 
 int chkSocket=0;
-char a[50];
 bool bConnect = false;
-
-
-void SendtoClient()
-{
-	while(true)
-   {
-        if(btnSocketSend)
-        {
-            if(chkSocket==1){
-                if (led==1 && led2==1) strcpy(a,"LED1 and LED2 is open");
-                else if(led==1 && led2==0)strcpy(a,"Only LED1 is open");
-                else if(led==0 && led2==1) strcpy(a,"Only LED2 is open");
-                else strcpy(a,"LED1 and LED2 is closed");
-                clt_sock.send(a,strlen(a));
-            }
-            led3=!led3;
-            Thread::wait(1000);
-        }
-    }
-}
-
 void main2()
 {
    while(true)
    {
-        if(bConnect==true)WatchdogRefresh();
+        if(bConnect==true) WatchdogRefresh();
         Thread::wait(1000);
     }
 }
 
 
-char path[255];
-char c[5000];
+
+// motor
+bool bRotateflag = true;
+int iState=0;  //0: not feed ; 1:feed
+int RightMostAngle = 10.0f;
+int LeftMostAngle = -10.0f;
+
+class CSg90 : public PwmOut
+{
+public:
+	CSg90(PinName pwmPin):PwmOut(pwmPin)
+	{
+		period_ms(20);// 50Hz to trigger SG90
+		fnSetRotateAngle(-45.0f);
+	}
+	void fnSetRotateAngle(float fAngle)
+	{
+		if (fAngle > RightMostAngle ) fAngle = RightMostAngle;
+		else if(fAngle < LeftMostAngle) fAngle = LeftMostAngle;        
+		
+		int iRotate = (- (fAngle - RightMostAngle) * 950.0f / RightMostAngle) + 500.0f;
+		pulsewidth_us(iRotate);
+	}
+};
+void main_motor() {
+		CSg90 mypwm(PB_10);
+		int fRotateInc = 0;
+		while(1) { // back and forward once
+			wait(0.1);
+			switch (iState)
+			{
+				case 0:
+						break;
+				case 1:
+						if(fRotateInc <= RightMostAngle && bRotateflag)
+						{
+								fRotateInc += 2;
+						}
+						else
+						{
+								bRotateflag = false;
+								fRotateInc -= 2;
+								if(fRotateInc <= LeftMostAngle)
+								iState = 0;
+						}
+						mypwm.fnSetRotateAngle((float)fRotateInc);
+						break;
+			}
+		}
+}
+
+
+
+// HTTP part
 namespace dts {
-		const std::string endl = "\r\n";
-		std::string response200 = "<!DOCTYPE html> <html> <body> <p>Click the button to trigger a function that will output 'Hello World' in a p element with id='demo'.</p> <button id='clicker'>Click me</button> <p id='demo'></p> <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script> <script> $('#clicker').click(function () { $.ajax({ method: 'POST', url: '/feed', data: 'test=123&hi=no', success: function (data) { console.log(data); document.getElementById('demo').innerHTML = data; }, error: function (err) { console.log(err); document.getElementById('demo').innerHTML = err; } }); }); </script> </body> </html> ";
+		char http_content[5000];
 		//std::string response200 = "<html><body><h1>Hello, World!</h1></body></html>\n";
+		std::string response200 = "<!DOCTYPE html> <html> <body> <p>Click the button to trigger a function that will output 'Hello World' in a p element with id='demo'.</p> <button id='clicker'>Click me</button> <p id='demo'></p> <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script> <script> $('#clicker').click(function () { $.ajax({ method: 'POST', url: '/feed', data: 'test=123&hi=no', success: function (data) { console.log(data); document.getElementById('demo').innerHTML = data; }, error: function (err) { console.log(err); document.getElementById('demo').innerHTML = err; } }); }); </script> </body> </html> ";
 		std::string response404 = "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL was not found on this server.</p></body></html>\n";
 		std::string responsePOST = "success!";
+		std::map<int, std::string> reason;
 }
-std::map<int, std::string> reason;
-
 void HttpResponse(int status, std::string &content) {
-	std::string res = "HTTP/1.1 ";
-
-	std::stringstream ss;
-	ss << status;
-	
-	res += ss.str() + " " + reason[status] + "\n";
-	res += "Content-Type: text/html\nConnection: Closed\n\n";
-	res += content;
-	clt_sock.send(res.c_str(), res.length());
-
+		std::string res = "HTTP/1.1 ";
+		std::stringstream ss;
+		ss << status; // int to sstream
+		res += ss.str() + " " + dts::reason[status] + "\n";
+		res += "Content-Type: text/html\nConnection: Closed\n\n";
+		res += content;
+		clt_sock.send(res.c_str(), res.length());
 }
 void HttpRequest (char *c) {
-	
 		std::string s(c), body;
-		//std::cout << "heyheyheyyyyyyy" << s << std::endl << std::endl;
-		body = s.substr(s.find("\r\n\r\n")+4);
+		//std::cout << "DEBUG:" << s << std::endl << std::endl;
+		body = s.substr(s.find("\r\n\r\n")+4); // get post data
 		s = s.substr(0, s.find('\n'));
 		std::stringstream ss(s);
 		std::string method, uri, version;
@@ -97,16 +116,20 @@ void HttpRequest (char *c) {
 		std::cout << "Body: " << body << std::endl;
 		
 		if ( method == "GET" ) {
-			if (uri == "/") {
-				HttpResponse(200, dts::response200);
-				//clt_sock.send(dts::response200.c_str(), dts::response200.length());
-			} else {
-				HttpResponse(404, dts::response404);
-				//clt_sock.send(dts::response404.c_str(), dts::response404.length());
-			}
+				if (uri == "/") {
+						HttpResponse(200, dts::response200);
+				} else {
+						HttpResponse(404, dts::response404);
+				}
 		} else if ( method == "POST" ) {
 			if (uri == "/feed") {
-				HttpResponse(200, dts::responsePOST);
+					HttpResponse(200, dts::responsePOST);
+					if(iState == 0){
+							iState = 1;
+							bRotateflag = true;
+					}else{
+							iState = 0;
+					}
 			} else {
 				HttpResponse(404, dts::response404);
 			}
@@ -115,30 +138,29 @@ void HttpRequest (char *c) {
 
 int main()
 {
-		reason[200] = "OK";
-		reason[404] = "Not Found";
-    Thread t1;
-    Thread t2(osPriorityLow,DEFAULT_STACK_SIZE,NULL);
+		// add
+		dts::reason[200] = "OK";
+		dts::reason[404] = "Not Found";
+		Thread motor;
+	  motor.start(callback(main_motor));	
+		//
 	
+    Thread t1; // t1 for watch dog
     printf("Basic TCP server example\r\n");
-
     int ret=1;
     WatchdogInit();
     EthernetInterface eth;
     ret=eth.connect();
-    
     if (ret==0) {
-        bConnect=true;
+        bConnect = true;
         t1.start(callback(main2));
         
         printf("The target IP address is '%s'\r\n", eth.get_ip_address());
         
         /* Open the server on ethernet stack */
         srv.open(&eth);
-        
         /* Bind the HTTP port (TCP 80) to the server */
         srv.bind(80);
-        
         /* listening connections */
         srv.listen();
         
@@ -146,74 +168,22 @@ int main()
             ret=srv.accept(&clt_sock, &clt_addr);
             if (ret==0)
             {
-							  t2.start(callback(SendtoClient));
-								
                 pc.printf("Connect Sucess!!\r\n");
                 pc.printf("Accept from %s:%d\r\n", clt_addr.get_ip_address(), clt_addr.get_port());
                 chkSocket=1;
                 ret =-1;
-
-								//clt_sock.send("Hello_Client",strlen("Hello_Client"));
-
                 if (ret!=0)
                 {
-                    ret = clt_sock.recv(c, sizeof(c)); //recv how many byte of data
-                    pc.printf("%d : \r\n",ret);
-
-										HttpRequest(c);
-
+										ret = clt_sock.recv(dts::http_content, sizeof(dts::http_content)); //recv how many byte of data
+                    // pc.printf("%d : \r\n",ret); // print data size
+										// add
+										pc.printf("Our Result: \r\n");
+										HttpRequest(dts::http_content);
                 }
                 pc.printf("%s is closed!\r\n\n",clt_addr.get_ip_address());
                 chkSocket=0;
-								t2.terminate();
                 clt_sock.close();      
             }
-            
-          
-            
         }
     }
 }
-
-	
-		/*
-		char str[] = "HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Hello, World!</h1></body></html>\n";
-		char str404[] = "HTTP/1.1 404 Not Found\nConnection: Closed\nContent-Type: text/html; charset=iso-8859-1\n\n<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\"><html><head><title>404 Not Found</title></head><body><h1>Not Found</h1><p>The requested URL was not found on this server.</p></body></html>\n";
-
-		char *space = strstr(strstr(c, "GET"), " ") + 1;
-		memset(path, 0, sizeof(path)); // clear path var
-		strncpy(path, space, strstr(space, " ") - space);
-		printf("Requesting: %s\r\n", path);
-		if (strcmp(path, "/") == 0) {
-				clt_sock.send(str, strlen(str));
-		} else {
-				clt_sock.send(str404, strlen(str404));
-		}
-		*/
-
-
-/*
-int GETindex = 0;
-int HTTPindex = 0;
-for(int i = 0;i < ret;i++) {
-		if (c[i] == 'G' && c[i+1] == 'E' && c[i+2] == 'T')
-				GETindex = i+3;
-		if (c[i] == 'H' && c[i+1] == 'T' && c[i+2] == 'T')
-				HTTPindex = i-1;
-}
-for(int i = GETindex; i < HTTPindex; i++){
-		pc.printf("%c",c[i]);
-}
-
-
-
-                    char *space = strstr(strstr(c, "GET"), " ") + 1;
-                    memset(path, 0, sizeof(path)); // clear path var
-                    strncpy(path, space, strstr(space, " ") - space);
-                    printf("Requesting: %s\r\n", path);
-                    if (strcmp(path, "/") == 0) {
-                        clt_sock.send(str, strlen(str));
-                    } else {
-                        clt_sock.send(str404, strlen(str404));
-                    }
-*/
